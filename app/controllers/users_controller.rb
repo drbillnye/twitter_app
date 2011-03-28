da38228bb12fa8ffed5a68d205cfcf0df15ee45a
Al Doan <drbillnye@gmail.com>
2011-03-27 20:37:44 -0400

    user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,19 @@ class UsersController < ApplicationController
 	@title = @user.name
   end
   def new
+	@user = User.new
 	@title = "Sign Up"
+	
   end
-
+  def create
+	@user = User.new(params[:user])
+	if @user.save
+	 flash[:success] = "Welcom to the Twitter App BABY!"
+	 redirect_to @user
+	else
+	  @title = "Sign Up"
+	  flash[:error] = "What the garbage man!"
+	  render 'new'
+	end
+  end
 end
